5fc79d7afdca6485a54b4b0722426ac5a92bc813
harnusek <ondrej.harnusek@gmail.com>
2019-03-25 22:06:34 +0100

    Update tool_synonyms.py

--- core/tool_synonyms.py
+++ core/tool_synonyms.py
@@ -4,11 +4,12 @@ import requests
 from lxml.etree import tostring
 
 def get_similarity(sent1,sent2):
-    return list_synonym(sent1)
+    return synonym_list(sent1)
 
-def list_synonym(word):
+def synonym_list(word):
     url = 'https://slovnik.azet.sk/synonyma/?q=' + word
     page = requests.get(url)
     tree = html.fromstring(page.content)
-    terms = tree.xpath('//em[@class="term"]')
-    return [t.text_content() for t in terms]
+    elems = tree.xpath('//em[@class="term"]')
+    terms = [e.text_content() for e in elems]
+    return terms
