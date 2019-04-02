866a7113ddc308a87de4295b00b92a0c25e56fb9
harnusek <ondrej.harnusek@gmail.com>
2019-04-02 23:34:12 +0200

    all green

--- core/common.py
+++ core/common.py
@@ -4,15 +4,17 @@ import requests
 import pandas as pd
 import json
 
-POS_TAGSET = ['default','S','V','A']
+POS_TAGSET = ['default','P','V','S','Z']
 
 def preprocessing(sent_1,sent_2, del_stop, use_pos, use_lem):
     analysed_sent_1 = sentence_analysis(sent_1, del_stop, use_lem)
     analysed_sent_2 = sentence_analysis(sent_2, del_stop, use_lem)
     if(use_pos):
-        splited = split_pos(analysed_sent_1, analysed_sent_2, POS_TAGSET)
+        categorized = categorize_sentences(analysed_sent_1, analysed_sent_2, POS_TAGSET)
     else:
-        splited = split_pos(analysed_sent_1, analysed_sent_2, ['default'])
+        categorized = categorize_sentences(analysed_sent_1, analysed_sent_2, ['default'])
+    matrices = generate_matrices(categorized)
+    return matrices
 
 def sentence_analysis(sent, del_stop=False, use_lem=False):
     analysed_sent = list()
@@ -25,7 +27,7 @@ def sentence_analysis(sent, del_stop=False, use_lem=False):
         for token in part['tokens']:
             # needs test stop words
             word = token['lemma'] if use_lem else token['text']
-            tag = token['tag'] # needs simplify
+            tag = token['tag'][0] # needs simplify
             pair = {'word':word, 'tag':tag}
             analysed_sent.append(pair)
     return analysed_sent
@@ -36,29 +38,40 @@ def sentence_analysis(sent, del_stop=False, use_lem=False):
 #     tokens = str.split(' ')
 #     return tokens
 
-# def remove_stop_words(words):
+# def is_stop_word(word):
 #     with open('core/data/stop_words_SK.txt', 'r', encoding='utf-8') as file:
 #         stop_words = [word.rstrip() for word in file]
 #         filtered_words = [word for word in words if word not in stop_words]
 #         return filtered_words
 
-def split_pos(analysed_sent_1, analysed_sent_2, categories):
-    splited = [[[],[]]] * size(categories)
+def categorize_sentences(analysed_sent_1, analysed_sent_2, categories):
+    splited = [[list(),list()] for _ in categories]
     for pair in analysed_sent_1:
-        index = categories.index(pair['tag'])
-        splited[index][0] = pair['word']
+        index = 0
+        if pair['tag'] in categories:
+            index = categories.index(pair['tag'])
+        # print(pair['word'], pair['tag'], index, splited[index])
+        splited[index][0].append(pair['word'])
     for pair in analysed_sent_2:
-        index = categories.index(pair['tag'])
-        splited[index][1] = pair['word'
+        index = 0
+        if pair['tag'] in categories:
+            index = categories.index(pair['tag'])
+        splited[index][1].append(pair['word'])
     return splited
 
-def generate_matrices(tokens1, tokens2): # TODO pos tags
-    for tag in tag_list:
-        df = pd.DataFrame(columns=tokens1, index=tokens2)
-        df = df.apply(pd.to_numeric, errors='coerce')
-        matrices.append(df)
+def generate_matrices(categorized):
+    matrices = list()
+    for columns, index in categorized:
+        if(len(columns)>0 and len(index)>0):
+            df = pd.DataFrame(columns=columns, index=index)
+            df = df.apply(pd.to_numeric, errors='coerce')
+            matrices.append(df)
+        else:
+            matrices.append(None)
     return matrices
 
 # post
 def avg_list(list):
-    return sum(list)/len(list)
+    suma = sum([i for i in list if i is not None])
+    lenth = len([i for i in list if i is not None])
+    return suma/lenth
--- tests/test_common.py
+++ tests/test_common.py
@@ -6,54 +6,71 @@ import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'core'))
 from common import preprocessing
 from common import sentence_analysis
+from common import categorize_sentences
 
-from common import avg_list
 # from common import lemmatization
 from common import generate_matrices
-# from common import remove_stop_words
+# from common import is_stop_word
+from common import avg_list
 
 class TestCommon(unittest.TestCase):
 # pre
     def test_preprocessing(self):
-        return
-        sent_1 = "Koľko je hodín?"
-        sent_2 = "Aký je čas?"
+        sent_1 = "Koľko dní?"
+        sent_2 = "Aký je?"
         del_stop = False
-        use_pos = False
+        use_pos = True
         use_lem = False
-        print(preprocessing(sent_1,sent_2, del_stop, use_pos, use_lem))
+        matrices = [None,
+                    pd.DataFrame(columns=['Koľko'], index= ['Aký']).apply(pd.to_numeric, errors='coerce'),
+                    None,
+                    None,
+                    pd.DataFrame(columns=['?'], index=['?']).apply(pd.to_numeric, errors='coerce')]
+        for e, a in zip(matrices, preprocessing(sent_1,sent_2, del_stop, use_pos, use_lem)):
+            if(e is not None and a is not None):
+                self.assertTrue(pd.DataFrame.equals(e, a))
 
     def test_sentence_analysis(self):
         sent = "anotátor, napr. tento!"
-        analyzed_sent = [{'word': 'anotátor', 'tag': 'SSms1'},
+        analyzed_sent = [{'word': 'anotátor', 'tag': 'S'},
                          {'word': ',', 'tag': 'Z'},
                          {'word': 'napr', 'tag': 'W'},
                          {'word': '.', 'tag': 'Z'},
-                         {'word': 'tento', 'tag': 'PFms1'},
+                         {'word': 'tento', 'tag': 'P'},
                          {'word': '!', 'tag': 'Z'}]
         self.assertEqual(analyzed_sent, sentence_analysis(sent))
 
-    def test_remove_stop_words(self):
-        os.chdir('..')
-        self.assertEqual(['1', '2'], remove_stop_words(['1', '2','a']))
+    def test_categorize_sentences(self):
+        analyzed_sent1 = [{'word': '0', 'tag': 'A'},
+                          {'word': '1', 'tag': 'B'},
+                          {'word': '2', 'tag': 'B'}]
+        analyzed_sent2 = [{'word': '3', 'tag': 'A'},
+                          {'word': '4', 'tag': 'B'},
+                          {'word': '5', 'tag': 'X'}]
+        categories = ['default', 'A', 'B', 'C']
+        categorized = [[[], ['5']], [['0'], ['3']], [['1', '2'], ['4']], [[], []]]
+        self.assertEqual(categorized, categorize_sentences(analyzed_sent1, analyzed_sent2, categories))
 
-    def test_lemmatization(self):
-        self.assertEqual([''], lemmatization(''))
-        self.assertEqual(['mama', 'alebo', 'nemať'], lemmatization('mám, alebo nemám!'))
-        self.assertEqual(['alfa', 'beta'], lemmatization('alfa beta?'))
-
-# post
-    def test_avg_list(self):
-        self.assertEqual(0, avg_list([0]))
-        self.assertEqual(9, avg_list([9]))
-        self.assertEqual(2, avg_list([0, 1, 2, 3, 4]))
+    # def test_is_stop_word(self):
+    #     os.chdir('..')
+    #     self.assertEqual(['1', '2'], is_stop_word(['1', '2','a']))
+    #
+    # def test_lemmatization(self):
+    #     self.assertEqual([''], lemmatization(''))
+    #     self.assertEqual(['mama', 'alebo', 'nemať'], lemmatization('mám, alebo nemám!'))
+    #     self.assertEqual(['alfa', 'beta'], lemmatization('alfa beta?'))
 
     def test_generate_matrices(self):
         tokens1 = ["ako","si","starý"]
         tokens2 = ["koľko","máš","rokov"]
+        categorized = [[tokens1, tokens2]]
         matrices = [pd.DataFrame(columns=tokens1, index= tokens2).apply(pd.to_numeric, errors='coerce')]
-        for e, a in zip(matrices, generate_matrices(tokens1, tokens2)):
+        for e, a in zip(matrices, generate_matrices(categorized)):
             self.assertTrue(pd.DataFrame.equals(e, a))
-
+# post
+    def test_avg_list(self):
+        self.assertEqual(0, avg_list([0, None]))
+        self.assertEqual(9, avg_list([9]))
+        self.assertEqual(2, avg_list([0, None, 1, 2, 3, None, None, 4]))
 if __name__ == '__main__':
     unittest.main()
