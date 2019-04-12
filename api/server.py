d851c9e37768977199dfa3509ba544869c50d538
Ondrej Harnúšek <ondrej.harnusek@gmail.com>
2019-04-12 11:23:24 +0200

    Update server.py

--- api/server.py
+++ api/server.py
@@ -14,7 +14,7 @@ from logging.handlers import RotatingFileHandler
 app = connexion.App(__name__, specification_dir="./")
 
 # Read the swagger.yml file to configure the endpoints
-# app.add_api("swagger.yml")
+app.add_api("swagger.yml")
 
 
 # create a URL route in our application for "/"
