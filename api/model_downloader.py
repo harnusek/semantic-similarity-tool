5cf2ca6cc36bebc1f990e94d003e6229d21dcbaa
harnusek <ondrej.harnusek@gmail.com>
2019-04-14 21:11:21 +0200

    Create model_downloader.py

---
+++ api/model_downloader.py
@@ -0,0 +1,33 @@
+import requests
+
+file_id = '157-3PiLe3pJSiSPHvkDKT7AB5S1Mxz8Q'
+destination = '[SK]prim-6.1-public-all.shuffled.080cbow.bin'
+
+def download_file_from_google_drive():
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : file_id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
